5fc9a0d548e92a5879e355198c0bba550bf95971
Hazem Emam <Hazem.emam20150198@yahoo.com>
2018-04-08 20:11:10 +0200

    a

---
+++ GP/Models/students.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GP.Models
+{
+    public class students
+    {
+        [Key]
+        public int student_id { get; set; }
+        public string student_name { get; set; }
+        public int student_ssn { get; set; }
+        public float student_gpa { get; set; }
+        public string transcript { get; set; }
+        public virtual ICollection<teamLeader> teamleader { get; set; }
+    }
+}
