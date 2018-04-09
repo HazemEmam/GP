b298ef5d9bef5367a75de59d324a4ceee0e986ae
hager Ali <38143697+hagerAli194@users.noreply.github.com>
2018-04-08 19:49:34 -0700

    Rehab & Hager
    Add 3 Models (students, Project Idea & Team Leader) For Student Actor

---
+++ Models/projectIdea.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GP.Models
+{
+    public class projectIdea
+    {
+        [Key]
+        public int id { get; set; }
+        public string project_name { get; set; }
+        public string project_description { get; set; }
+        public virtual ICollection<teamLeader> teamleader { get; set; }
+    }
+}
---
+++ Models/teamLeader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GP.Models
+{
+    public class teamLeader
+    {
+        [Key]
+        public int teamLeader_id { get; set; }
+        public string teamLeader_password { get; set; }
+    }
+}
